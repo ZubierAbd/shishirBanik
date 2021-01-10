52844f56705f1de87c572f7a79c4e9786a261a75
Zubier <zubierabd@gmail.com>
2021-01-10 16:42:10 -0500

    made minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shishir';
+
+  numberOfSpins: number = 0;
+  otherText: string = "time";
+  ShishirUrls = [];
+  shishirText = "";
+  ShishirQuotes = [
+    "The only salvation in life is through Shishir", "Once Shishir always Shishir", "Round is a shape", "Only Shishir is real", "The truth lies in the gaze of Shishir"
+  ]
+
+
+  ngOnInit() {
+    this.numberOfSpins = 0;
+
+    setInterval(() => {
+      this.numberOfSpins = this.numberOfSpins + 1;
+      if (this.numberOfSpins != 0) {
+        this.otherText = "times";
+      }
+    }, 3000)
+
+    this.shishirText = this.ShishirQuotes[Math.floor(Math.random() * 5) - 1];
+
+    setInterval(() => {
+      this.shishirText = this.ShishirQuotes[Math.floor(Math.random() * 5) - 1]
+    }, 5000);
+
+  }
 }
